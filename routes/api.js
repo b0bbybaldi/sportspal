4dff19a2fb4380b52c8d22e7a8e7d60d0d950aa0
James Reinke <jamesreinke4@gmail.com>
2018-06-13 13:49:51 -0700

    updating hash function

--- routes/api.js
+++ routes/api.js
@@ -1,11 +1,11 @@
 var db = require('../models');
 
 
-function hash () {
+function hash (str) {
   var hash = 0, i, chr;
-  if (this.length === 0) return hash;
-  for (i = 0; i < this.length; i++) {
-    chr   = this.charCodeAt(i);
+  if (str.length === 0) return hash;
+  for (i = 0; i < str.length; i++) {
+    chr   = str.charCodeAt(i);
     hash  = ((hash << 5) - hash) + chr;
     hash |= 0; // Convert to 32bit integer
   }
